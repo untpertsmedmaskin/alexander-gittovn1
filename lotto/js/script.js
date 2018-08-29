46a0888b664d07a4a337f5259c686acc76c8ca50
untpertsmedmaskin <alexander.krogius@gmail.com>
2018-08-29 09:54:23 +0300

    script.js gjord och länkad till index.html

--- lotto/js/script.js
+++ lotto/js/script.js
@@ -1,11 +1,14 @@
 //slumptal mellan 1 och 39
 //OBS dålig kod, mjlighet till dublikat => använd inte ;)
 function lottoRad() {
+	
+	alert("Hello! I am an alert box!!");
+	
 	for (var i=0; i<=7; i++) {
 
 		let lottery = Math.floor(Math.random() * 38)+1;
 	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
-		alert("Hello! I am an alert box!!");
+		
 
 	}
 }
